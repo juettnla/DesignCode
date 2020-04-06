d6b69e5e99d59ed454292c958c117b13d2b595b0
Lars <l.juettner@gmx.de>
2020-04-06 12:28:03 +0200

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Lars Juettner on 06.04.20.
+//  Copyright © 2020 Lars Juettner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
